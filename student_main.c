da66e368067532fc44515e0299381b283e86fa80
sonaatashova <sonaatashova1@gmail.com>
2026-03-13 17:49:14 +0400

    Update student_main.c

--- student_main.c
+++ student_main.c
@@ -25,11 +25,17 @@ int main() {
          3. Store the common numbers in the 'intersection' array.
     */
     //----------------------------------------------------------------//
-    
     for(i = 0; i < n1; i++) {
         for(j = 0; j < n2; j++) {
             if(set1[i] == set2[j]) {
-                if(!exists(intersection, count, set1[i])) {
+                int duplicate = 0;
+                for(k = 0; k < count; k++) {
+                    if(intersection[k] == set1[i]) {
+                        duplicate = 1;
+                        break;
+                    }
+                }
+                if(!duplicate) {
                     intersection[count++] = set1[i];
                 }
             }
